c30d19496902cb3a6599887980c1da25b6b2e468
ksrgRei <839400662@qq.com>
2023-08-15 18:51:31 +0800

    项目初始化-引入Vant组件库

--- src/main.js
+++ src/main.js
@@ -3,8 +3,17 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// 加载Vant核心组件库
+import Vant from "vant";
+
+// 加载Vant全局样式
+import "vant/lib/index.css";
+
 // 加载全局样式
 import "./style/style.less";
+
+// 注册使用Vant组件库
+Vue.use(Vant);
 
 Vue.config.productionTip = false;
 
